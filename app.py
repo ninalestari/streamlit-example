34ddb3f1837de7dfbff5ea70567a8e535970bd89
ninalestari <75825567+ninalestari@users.noreply.github.com>
2023-11-29 12:09:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,32 +14,7 @@ st.write('Welcome to my web page!')
 # Function to create and display the animation
 create_animation()
 
-# Define the function
-def f(x):
-    return x**2 + 3*x + 5
 
-# Generate x values
-x = np.linspace(-10, 10, 400)
-
-# Create a figure and axis
-fig, ax = plt.subplots()
-
-# Set up the plot
-line, = ax.plot(x, f(x), 'r-', linewidth=2)
-ax.grid(True)
-plt.title('Graph of f(x) = x^2 + 3x + 5')
-
-# Define the animation function
-def animate(i):
-    ax.set_xlim(-10, 10)
-    ax.set_ylim(0, 100)
-    ax.fill_between(x, 0, f(x), where=(x < i), color='blue', alpha=0.3)
-
-# Create the animation
-ani = FuncAnimation(fig, animate, frames=np.linspace(-10, 10, 100), interval=100)
-
-# Show the animation
-plt.show()
 
 
 # Add more Streamlit commands to create your web page
