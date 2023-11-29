345cedc84aa7e300a4a9fba7855185f4c616b4a2
ninalestari <75825567+ninalestari@users.noreply.github.com>
2023-11-29 15:26:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
-# my_web_page.py
-
 import streamlit as st
-from your_animation_script import create_animation  # Import the function that creates the animation
 
+def main():
+    # Title for the app
+    st.title('Interactive Function Plot')
+    st.write('Welcome to my web page!')
 
-# Title for the app
-st.title('Interactive Function Plot')
-st.write('Welcome to my web page!')
-
-# Function to create and display the animation
-create_animation()
-
-
-
-
-# Add more Streamlit commands to create your web page
-
+if __name__ == "__main__":
+    main()
