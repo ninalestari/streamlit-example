aa1bf0b41ced6b0786b045d661e59193ed1124c1
ninalestari <75825567+ninalestari@users.noreply.github.com>
2023-11-29 12:03:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 # my_web_page.py
 
 import streamlit as st
+def main():
+    st.title('My First Web Page with Streamlit')
+    st.write('Welcome to my web page!')
 
-st.title('My First Web Page with Streamlit')
-st.write('Welcome to my web page!')
+if __name__ == '__main__'
+    main()
 
 # Add more Streamlit commands to create your web page
 
